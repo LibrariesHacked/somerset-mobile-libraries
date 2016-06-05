78674a1f3cfe2de2c36b5eb08cd7192d4ffbf12b
Dave <david_rowe@bathnes.gov.uk>
2016-06-06 00:42:03 +0100

    Geocoder work

--- scripts/geocode.py
+++ scripts/geocode.py
@@ -1,10 +1,14 @@
 import csv
-import urllib
+import requests # pip install requests
 import json
+import time
 
-with open('data/somersetmobiles.csv', 'rb') as mobilecsv:
+with open('../data/somersetmobiles.csv', 'r') as mobilecsv:
     reader = csv.reader(mobilecsv, delimiter=',', quotechar='"')
     next(reader, None)  # skip the headers
+    writer = csv.writer(open('../data/somersetmobiles_geocoded.csv', 'w'), delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(['Id','Mobile','Route','Day','StartDate','Location','Postcode','Start','End','Address','Lat','Lng'])
+    
     for row in reader:
         # Id,Mobile,Route,Day,StartDate,Location,Postcode,Start,End
         id = row[0]
@@ -16,9 +20,10 @@ with open('data/somersetmobiles.csv', 'rb') as mobilecsv:
         postcode = row[6]
         start = row[7]
         end = row[8]
-        url = ''
-        response = urllib.urlopen(url)
-        data = json.loads(response.read())
+        # use the postcode in the geocoder.  will most likely have to manually edit a few later anyway.
+        url = 'http://nominatim.openstreetmap.org/search/' + postcode + '?format=json&addressdetails=1&limit=1'
+        result = requests.get(url).json()
         
+        writer.writerow([id,mobile,route,day,startdate,location,postcode,start,end,result[0]['display_name'],result[0]['lat'],result[0]['lon']])
         # because the web service is rate limited, wait for a second before moving onto the next one.
         time.sleep(1)
