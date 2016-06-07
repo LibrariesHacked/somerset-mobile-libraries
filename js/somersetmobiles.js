a7126f49c4260417095719660a38b1eae3f3de9e
Dave <david_rowe@bathnes.gov.uk>
2016-06-07 15:59:27 +0100

    Cleanup work

--- js/somersetmobiles.js
+++ js/somersetmobiles.js
@@ -1,7 +1,5 @@
 ﻿var SomersetMobiles = {
-
     data: {},
-
     /////////////////////////////////////
     // Function: loadData
     // Loads the data asynchonously.
@@ -10,25 +8,8 @@
         Papa.parse('/data/somersetmobiles_geocoded.csv', {
             download: true,
             complete: function (results) {
-
-                // Now we start adding some additional data.
-                // We need the next datetime for each stop.
                 stops = {};
-
                 $.each(results.data.splice(1), function (key, val) {
-                    // Each library is on a timescale of once every 4 weeks.
-                    // Add 4 weeks onto the first stop time until we get a datetime in the future.
-                    var strStartDate = val[4] + ' ' + val[7];
-                    var nextDateTime = moment(strStartDate, 'MM/DD/YYYY hh:mm');
-                    var strEndDate = val[4] + ' ' + val[8];
-                    var nextDateTimeEnd = moment(strEndDate, 'MM/DD/YYYY hh:mm');
-                    var now = moment();
-                    while (now > nextDateTime) {
-                        nextDateTime.add(4, 'weeks');
-                    }
-                    while (now > nextDateTimeEnd) {
-                        nextDateTimeEnd.add(4, 'weeks');
-                    }
                     stops[val[0]] = {};
                     stops[val[0]]['Library'] = val[1];
                     stops[val[0]]['RouteId'] = val[1] + val[2];
@@ -43,24 +24,54 @@
                     stops[val[0]]['Town'] = val[10];
                     stops[val[0]]['Lat'] = val[11];
                     stops[val[0]]['Lng'] = val[12];
-                    stops[val[0]]['Due'] = nextDateTime.fromNow();
-                    stops[val[0]]['DueSystem'] = nextDateTime.format();
-                    stops[val[0]]['Duration'] = nextDateTimeEnd.diff(nextDateTime, 'minutes');
-
-                })
+                });
                 this.data = stops;
+                this.setDueDates();
                 callback(results.data);
             }.bind(this)
         })
     },
-
-    /////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////
+    // Function: SetDueDates
+    // Get the current id stop for the mobile library.
+    /////////////////////////////////////////////////////////////////////////////
+    setDueDates: function (mobile) { 
+        var now = moment();
+        $.each(this.data, function (key, val) {
+            // Each library is on a timescale of once every 4 weeks.
+            // Add 4 weeks onto the first stop time until we get a datetime in the future.
+            var strStartDate = val[4] + ' ' + val[7];
+            var nextDateTime = moment(strStartDate, 'MM/DD/YYYY hh:mm');
+            var strEndDate = val[4] + ' ' + val[8];
+            var nextDateTimeEnd = moment(strEndDate, 'MM/DD/YYYY hh:mm');
+            while (now > nextDateTime) nextDateTime.add(4, 'weeks');
+            while (now > nextDateTimeEnd) nextDateTimeEnd.add(4, 'weeks');
+            this.data[key]['Due'] = nextDateTime.fromNow();
+            this.data[key]['DueSystem'] = nextDateTime.format();
+            this.data[key]['Departing'] = nextDateTime.format();
+            this.data[key]['DepartingSystem'] = nextDateTime.format();
+            this.data[key]['Duration'] = nextDateTimeEnd.diff(nextDateTime, 'minutes');
+        }.bind(this));
+    },
+    /////////////////////////////////////////////////////////////////////////////
     // Function: GetCurentLocation
     // Get the current id stop for the mobile library.
-    /////////////////////////////////////
-    getCurrentLocation: function (mobile) { },
-
-
+    /////////////////////////////////////////////////////////////////////////////
+    getCurrentLocation: function (mobile) { 
+        var timeNow = moment();
+        // Refresh the due dates.
+        this.setDueDates();
+        var currentLocation = null;
+        $.each(this.data, function (key, val) {
+            if (timeNow.isBefore(val.DepartingSystem) && date.isAfter(val.DueSystem)) return val.Id;
+        }.bind(this));
+    },
+    /////////////////////////////////////////////////////////////////////////////
+    // Function: GetDataTable
+    // Gets all the current data in the format suitable for DataTables display
+    // Input: None
+    // Output: DataArray
+    /////////////////////////////////////////////////////////////////////////////
     getDataTable: function () {
         var dataArray = [];
         $.each(this.data, function (key, val) {
@@ -79,36 +90,44 @@
 
     getExcelRoute: function (mobile, route) { },
 
-
-
-
+    /////////////////////////////////////////////////////////////////////////////
+    // Function: SetCurrentDistances
+    // Get the nearest stop to a particular location.
+    // Input: Lat/Lng
+    // Output: None
+    /////////////////////////////////////////////////////////////////////////////
     setCurrentDistances: function (lat, lng) {
         var distance = function (lat1, lon1, lat2, lon2, unit) {
-            var radlat1 = Math.PI * lat1 / 180
-            var radlat2 = Math.PI * lat2 / 180
-            var radlon1 = Math.PI * lon1 / 180
-            var radlon2 = Math.PI * lon2 / 180
-            var theta = lon1 - lon2
-            var radtheta = Math.PI * theta / 180
+            var radlat1 = Math.PI * lat1 / 180;
+            var radlat2 = Math.PI * lat2 / 180;
+            var radlon1 = Math.PI * lon1 / 180;
+            var radlon2 = Math.PI * lon2 / 180;
+            var theta = lon1 - lon2;
+            var radtheta = Math.PI * theta / 180;
             var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-            dist = Math.acos(dist)
-            dist = dist * 180 / Math.PI
-            dist = dist * 60 * 1.1515
-            if (unit == "K") { dist = dist * 1.609344 }
-            if (unit == "N") { dist = dist * 0.8684 }
-            return dist
+            dist = Math.acos(dist);
+            dist = dist * 180 / Math.PI;
+            dist = dist * 60 * 1.1515;
+            if (unit == "K") dist = dist * 1.609344;
+            if (unit == "N") dist = dist * 0.8684;
+            return dist;
         };
-        
         $.each(this.data, function(key,val){
             val.CurrentDistance = distance(lat, lng, val.Lat, val.Lng);
             this.data[key] = val;
         }.bind(this));
+        return;
     },
-
+    /////////////////////////////////////////////////////////////////////////////
+    // Function: GetNearest
+    // Get the nearest stop to a particular location.
+    // Input: None
+    // Output: The Id of the stop.
+    /////////////////////////////////////////////////////////////////////////////
     getNearest: function () {
         var currentNearest = null;
         $.each(this.data, function (key, val) {
-            if (currentNearest == null || currentNearest.CurrentDistance > val.CurrentDistance) currentNearest = val;
+            if (currentNearest == null || currentNearest.CurrentDistance > val.CurrentDistance) currentNearest = key;
         });
         return currentNearest;
     }
