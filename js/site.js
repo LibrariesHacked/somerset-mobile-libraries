2d520a96bed3032da88c8b9fdf3a9bfcc57fda13
Dave <david_rowe@bathnes.gov.uk>
2016-06-11 10:10:49 +0100

    Changing map layers to be based on routes.

--- js/site.js
+++ js/site.js
@@ -48,80 +48,88 @@ $(function () {
             var tauntonCurrent = SomersetMobiles.getCurrentLocation('Taunton');
             var tauntonNext = SomersetMobiles.getNextLocation('Taunton');
             if (tauntonCurrent != null) {
-                $('#spTauntonCurrentPosition').html('<span class="lead">Currently at <strong>' + SomersetMobiles.data[tauntonCurrent]['Location'] + ', ' + SomersetMobiles.data[tauntonCurrent]['Town'] + '</strong> for another ' + moment(SomersetMobiles.data[tauntonCurrent]['DepartingSystem']).diff(moment(), 'minutes') + ' minutes</span>');
+                $('#spTauntonCurrentPosition').html('<span class="lead">Currently at ' + SomersetMobiles.data[tauntonCurrent]['Location'] + ', ' + SomersetMobiles.data[tauntonCurrent]['Town'] + ' for another ' + moment(SomersetMobiles.data[tauntonCurrent]['DepartingSystem']).diff(moment(), 'minutes') + ' minutes</span>');
             } else {
-                $('#spTauntonCurrentPosition').html('<span class="lead">Arriving at <strong>' + SomersetMobiles.data[tauntonNext]['Location'] + ', ' + SomersetMobiles.data[tauntonNext]['Town'] + '</strong> ' + SomersetMobiles.data[tauntonNext]['Due'] + '</span>');
+                $('#spTauntonCurrentPosition').html('<span class="lead">Arriving at ' + SomersetMobiles.data[tauntonNext]['Location'] + ', ' + SomersetMobiles.data[tauntonNext]['Town'] + ' ' + SomersetMobiles.data[tauntonNext]['Due'] + '</span>');
             }
             var wellsCurrent = SomersetMobiles.getCurrentLocation('Wells');
             var wellsNext = SomersetMobiles.getNextLocation('Wells');
             if (wellsCurrent != null) {
-                $('#spWellsCurrentPosition').html('<span class="lead">Currently at <strong>' + SomersetMobiles.data[wellsCurrent]['Location'] + ', ' + SomersetMobiles.data[wellsCurrent]['Town'] + '</strong> for another ' + moment(SomersetMobiles.data[wellsCurrent]['DepartureSystem']).diff() + ' minutes</span>');
+                $('#spWellsCurrentPosition').html('<span class="lead">Currently at ' + SomersetMobiles.data[wellsCurrent]['Location'] + ', ' + SomersetMobiles.data[wellsCurrent]['Town'] + ' for another ' + moment(SomersetMobiles.data[wellsCurrent]['DepartingSystem']).diff(moment(), 'minutes') + ' minutes</span>');
             } else {
-                $('#spWellsCurrentPosition').html('<span class="lead">Arriving at <strong>' + SomersetMobiles.data[wellsNext]['Location'] + ', ' + SomersetMobiles.data[wellsNext]['Town'] + '</strong> ' + SomersetMobiles.data[wellsNext]['Due'] + '</span>');
+                $('#spWellsCurrentPosition').html('<span class="lead">Arriving at ' + SomersetMobiles.data[wellsNext]['Location'] + ', ' + SomersetMobiles.data[wellsNext]['Town'] + ' ' + SomersetMobiles.data[wellsNext]['Due'] + '</span>');
             }
         };
         setCurrentPositions();
-
-
-        var markerArrayTaunton = [];
-        var markerArrayWells = [];
-        var markersAllBounds = [];
-        var markersTivertonBounds = [];
-        var markersWellsBounds = [];
+        var markersArrays = {};
+        var markersBounds = {};
         $.each(SomersetMobiles.data, function (key, val) {
             // Add items to the map.
             if (val.Lat && val.Lat != 'TODO') {
-                markersAllBounds.push([val.Lat, val.Lng]);
                 var popup = L.popup({
                     maxWidth: 160,
                     maxHeight: 140,
                     closeButton: false,
                     className: ''
-                }).setContent('<p>' + val.Location + ', ' + val.Town + '</p><small><strong>Date </strong>' + moment(val.DueSystem).format() + '</small>');
+                }).setContent('<p>' + val.Location + ', ' + val.Town + '</p><strong>Date </strong>' + moment(val.DueSystem).format('DD MMM YYYY hh:mm') + '');
                 var className = 'taunton';
+
+                // Set up the associative arrays for layers and bounds
+                if (!markersArrays[val.RouteId]) markersArrays[val.RouteId] = [];
+                if (!markersBounds[val.RouteId]) markersBounds[val.RouteId] = [];
+
                 if (val.Library == 'Taunton') className = 'wells';
                 var stopIcon = L.divIcon({ html: '<div><span>' + val.Route + '</span></div>', className: "marker-cluster marker-cluster-" + className, iconSize: new L.Point(20, 20) });
-                if (val.Library == 'Taunton') {
-                    markerArrayTaunton.push(L.marker([val.Lat, val.Lng], { icon: stopIcon }).bindPopup(popup));
-                    markersTivertonBounds.push([val.Lat, val.Lng]);
-                }
-                if (val.Library == 'Wells') {
-                    markerArrayWells.push(L.marker([val.Lat, val.Lng], { icon: stopIcon }).bindPopup(popup));
-                    markersWellsBounds.push([val.Lat, val.Lng]);
-                }
+                markersArrays[val.RouteId].push(L.marker([val.Lat, val.Lng], { icon: stopIcon }).bindPopup(popup));
+                markersArrays[val.RouteId].push([val.Lat, val.Lng]);
             }
         });
-        var tauntonGroup = L.layerGroup(markerArrayTaunton);
-        var wellsGroup = L.layerGroup(markerArrayWells);
-        map.addLayer(tauntonGroup);
-        map.addLayer(wellsGroup);
-        map.fitBounds(markersAllBounds);
+        var layers = {};
+        layers['taunton'] = L.layerGroup($.map(markersArrays, function (val,key) {
+            if (key.indexOf('Taunton') != -1) return val;
+        }));
+        layers['wells'] = L.layerGroup($.map(markersArrays, function (val, key) {
+            if (key.indexOf('Wells') != -1) return val;
+        }));
+
+
+        map.addLayer(layers['taunton']);
+        map.addLayer(layers['wells']);
+        map.fitBounds($.map(markersBounds, function (key, val) {
+            return val;
+        }));
+
         var currentFilter = 'all';
+
+        // Set the stop counts
+        $('#spStopCount').text('1');
+        $('#spTauntonStopCount').text('2');
+        $('#spWellsStopCount').text('3');
 
         // Set up the option to show either set of library stops
         filterLibrariesMap = function (filter) {
             if (currentFilter == 'all' && filter == 'taunton') {
-                map.removeLayer(wellsGroup);
-                map.fitBounds(tauntonGroup);
+                map.removeLayer(layers['wells']);
+                //map.fitBounds(markersTauntonBounds);
             }
-            if (currentFilter == 'all' && filter == 'well') {
-                map.removeLayer(tauntonGroup);
-                map.addLayer(markerArrayWells);
-                map.fitBounds(wellsGroup);
+            if (currentFilter == 'all' && filter == 'wells') {
+                map.removeLayer(layers['taunton']);
+                map.addLayer(layers['wells']);
+                //map.fitBounds(markersWellsBounds);
             }
             if (currentFilter == 'wells' && filter == 'taunton') {
-                map.removeLayer(wellsGroup);
-                map.addLayer(tauntonGroup);
-                map.fitBounds(tauntonGroup);
+                map.removeLayer(layers['wells']);
+                map.addLayer(layers['taunton']);
+                //map.fitBounds(markersTauntonBounds);
             }
             if (currentFilter == 'taunton' && filter == 'wells') {
-                map.removeLayer(tauntonGroup);
-                map.addLayer(wellsGroup);
-                map.fitBounds(wellsGroup);
+                map.removeLayer(layers['taunton']);
+                map.addLayer(layers['wells']);
+                //map.fitBounds(markersTauntonBounds);
             }
             if (currentFilter == 'wells' && filter == 'all') {
-                map.addLayer(tauntonGroup);
-                map.fitBounds(markersAllBounds);
+                map.addLayer(layers['taunton']);
+                //map.fitBounds(markersAllBounds);
             }
             if (currentFilter == 'taunton' && filter == 'all') {
                 map.addLayer(wellsGroup);
